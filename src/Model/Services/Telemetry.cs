56c001e9ba70498a6e4a4016434ea3a7970c82c5
Rolf Michelsen <rolf@rolfmichelsen.com>
2025-02-16 13:54:35 +0100

    Add session counters to the telemetry service
    Add counters for the total number of sessions and the current number of
    sessions to the telemetry service.

--- src/Model/Services/Telemetry.cs
+++ src/Model/Services/Telemetry.cs
@@ -17,9 +17,42 @@ public class Telemetry : IDisposable
     public ActivitySource ActivitySource { get; } = new ActivitySource(ServiceName);
     private readonly Meter meter = new Meter(ServiceName);
 
+    /// <summary>Counter for the total number of sessions created.</summary>
+    private Counter<long> SessionsTotal { get; }
+
+    /// <summary>Counter for the current number of active sessions.</summary>
+    private UpDownCounter<long> SessionsActive { get; }
+
 
     /// <summary>Constructor.</summary>
-    public Telemetry() {}
+    public Telemetry()
+    {
+        SessionsTotal = meter.CreateCounter<long>("spacecowboy.sessions.total", "Total number of sessions created.");
+        SessionsActive = meter.CreateUpDownCounter<long>("spacecowboy.sessions.active", "Current number of active sessions.");
+    }
+
+
+    /// <summary>Increment the number of sessions created.</summary>
+    /// <param name="count">Number of sessions to increment by (default 1).</param>
+    public void IncrementSessions(int count = 1)
+    {
+        if (count < 0) {
+            throw new ArgumentOutOfRangeException(nameof(count), "Count must be a positive integer.");
+        }
+        SessionsTotal.Add(count);
+        SessionsActive.Add(count);
+    }
+
+
+    /// <summary>Decrement the number of active sessions.</summary>
+    /// <param name="count">Number of sessions to decrement by (default 1).</param>
+    public void DecrementSessions(int count = 1)
+    {
+        if (count < 0) {
+            throw new ArgumentOutOfRangeException(nameof(count), "Count must be a positive integer.");
+        }
+        SessionsActive.Add(-count);
+    }
 
 
     /// <summary>Properly dispose of resources.</summary>
